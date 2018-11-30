4ddc63b99334a71613ce3200ac97d87fa56adfe2
cuteant <cuteant@outlook.com>
2018-11-30 17:05:26 +0800

    Harden ref-counting concurrency semantics (#8583)
    Motivation
    
    This PR proposes a modified approach which provides roughly half the
    gains but stronger concurrency semantics. Race conditions still exist
    but their scope is narrowed to much less likely cases (releases
    coinciding with retain overflow), and even in those
    cases certain guarantees are still assured. Once release() returns true,
    all subsequent release/retains are guaranteed to throw, and in
    particular deallocate will be called at most once.
    
    Modifications
    
    - Use even numbers internally (including -ve) for live refcounts
    - "Final" release changes to odd number (equivalent to refcount 0)
    - Retain still uses faster getAndAdd, release uses CAS loop
    - First CAS attempt uses non-volatile read
    - Thread.yield() after a failed CAS provides a net gain
    
    Result
    
    More (though not completely) robust concurrency semantics for ref
    counting; increased latency under high contention, but still roughly
    twice as fast as the original logic. Bench results to follow

--- src/DotNetty.Buffers/AbstractReferenceCountedByteBuffer.cs
+++ src/DotNetty.Buffers/AbstractReferenceCountedByteBuffer.cs
@@ -5,22 +5,27 @@
 
 namespace DotNetty.Buffers
 {
+    using System.Runtime.CompilerServices;
     using System.Threading;
     using DotNetty.Common;
+    using DotNetty.Common.Utilities;
 
     public abstract class AbstractReferenceCountedByteBuffer : AbstractByteBuffer
     {
-        int referenceCount = 1;
+        // even => "real" refcount is (refCnt >>> 1); odd => "real" refcount is 0
+        int referenceCount = 2;
 
         protected AbstractReferenceCountedByteBuffer(int maxCapacity)
             : base(maxCapacity)
         {
         }
 
-        public override int ReferenceCount => Volatile.Read(ref this.referenceCount);
+        public override int ReferenceCount => RealRefCnt(Volatile.Read(ref this.referenceCount));
 
-        //An unsafe operation intended for use by a subclass that sets the reference count of the buffer directly
-        protected internal void SetReferenceCount(int value) => Interlocked.Exchange(ref this.referenceCount, value);
+        /// <summary>
+        /// An unsafe operation intended for use by a subclass that sets the reference count of the buffer directly
+        /// </summary>
+        protected internal void SetReferenceCount(int newRefCnt) => Interlocked.Exchange(ref this.referenceCount, newRefCnt << 1); // overflow OK here
 
         public override IReferenceCounted Retain() => this.Retain0(1);
 
@@ -33,19 +38,21 @@ namespace DotNetty.Buffers
 
         IReferenceCounted Retain0(int increment)
         {
-            int refCnt = Volatile.Read(ref this.referenceCount);
-            int oldRefCnt;
-            do
+            // all changes to the raw count are 2x the "real" change
+            int adjustedIncrement = increment << 1; // overflow OK here
+            int oldRef = this.GetAndAddRefCnt(adjustedIncrement);
+            if ((oldRef & 1) != 0)
             {
-                oldRefCnt = refCnt;
-                int nextCnt = refCnt + increment;
-
-                // Ensure we not resurrect (which means the refCnt was 0) and also that we encountered an overflow.
-                if (nextCnt <= increment) { ThrowHelper.ThrowIllegalReferenceCountException(refCnt, increment); }
-
-                refCnt = Interlocked.CompareExchange(ref this.referenceCount, nextCnt, refCnt);
-            } while (refCnt != oldRefCnt);
-
+                ThrowHelper.ThrowIllegalReferenceCountException(0, increment);
+            }
+            // don't pass 0!
+            if ((oldRef <= 0 && oldRef + adjustedIncrement >= 0) ||
+                (oldRef >= 0 && oldRef + adjustedIncrement < oldRef))
+            {
+                // overflow case
+                this.GetAndAddRefCnt(-adjustedIncrement);
+                ThrowIllegalRawReferenceCountException(oldRef, increment);
+            }
             return this;
         }
 
@@ -64,23 +71,128 @@ namespace DotNetty.Buffers
 
         bool Release0(int decrement)
         {
-            int refCnt = Volatile.Read(ref this.referenceCount);
+            int rawCnt = Volatile.Read(ref this.referenceCount);
+            int realCnt = ToLiveRealCnt(rawCnt, decrement);
+            if (decrement == realCnt)
+            {
+                if (this.CompareAndSetRefCnt(rawCnt, 1))
+                {
+                    this.Deallocate();
+                    return true;
+                }
+                return this.RetryRelease0(decrement);
+            }
+            return this.ReleaseNonFinal0(decrement, rawCnt, realCnt);
+        }
+
+        bool ReleaseNonFinal0(int decrement, int rawCnt, int realCnt)
+        {
+            if (decrement < realCnt
+                    // all changes to the raw count are 2x the "real" change
+                    && this.CompareAndSetRefCnt(rawCnt, rawCnt - (decrement << 1)))
+            {
+                return false;
+            }
+            return RetryRelease0(decrement);
+        }
+
+        bool RetryRelease0(int decrement)
+        {
+            var sw = new SpinWait();
+            int rawCnt = Volatile.Read(ref this.referenceCount);
+            int oldRawCnt;
+            while (true)
+            {
+                oldRawCnt = rawCnt;
+                int realCnt = ToLiveRealCnt(rawCnt, decrement);
+                if (decrement == realCnt)
+                {
+                    rawCnt = Interlocked.CompareExchange(ref this.referenceCount, 1, oldRawCnt);
+                    if (rawCnt == oldRawCnt)
+                    {
+                        this.Deallocate();
+                        return true;
+                    }
+                }
+                else if (decrement < realCnt)
+                {
+                    // all changes to the raw count are 2x the "real" change
+                    rawCnt = Interlocked.CompareExchange(ref this.referenceCount, rawCnt - (decrement << 1), oldRawCnt);
+                    if (rawCnt == oldRawCnt)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    ThrowHelper.ThrowIllegalReferenceCountException(realCnt, -decrement);
+                }
+                sw.SpinOnce(); // this benefits throughput under high contention
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        int GetAndAddRefCnt(int delta)
+        {
+            var refCnt = Volatile.Read(ref this.referenceCount);
             int oldRefCnt;
             do
             {
                 oldRefCnt = refCnt;
-
-                if (refCnt < decrement) { ThrowHelper.ThrowIllegalReferenceCountException(refCnt, -decrement); }
-
-                refCnt = Interlocked.CompareExchange(ref this.referenceCount, refCnt - decrement, refCnt);
+                refCnt = Interlocked.CompareExchange(ref this.referenceCount, refCnt + delta, oldRefCnt);
             } while (refCnt != oldRefCnt);
+            return oldRefCnt;
+        }
 
-            if (refCnt == decrement)
+        [MethodImpl(InlineMethod.Value)]
+        bool CompareAndSetRefCnt(int expect, int update)
+        {
+            return expect == Interlocked.CompareExchange(ref this.referenceCount, update, expect);
+        }
+
+        [MethodImpl(InlineMethod.Value)]
+        static int RealRefCnt(int rawCnt)
+        {
+            return (rawCnt & 1) != 0 ? 0 : rawCnt.RightUShift(1);
+        }
+
+        /// <summary>
+        /// Like <see cref="RealRefCnt(int)"/> but throws if refCnt == 0
+        /// </summary>
+        /// <param name="rawCnt"></param>
+        /// <param name="decrement"></param>
+        /// <returns></returns>
+        [MethodImpl(InlineMethod.Value)]
+        static int ToLiveRealCnt(int rawCnt, int decrement)
+        {
+            if ((rawCnt & 1) == 0)
             {
-                this.Deallocate();
-                return true;
+                return rawCnt.RightUShift(1);
             }
-            return false;
+            // odd rawCnt => already deallocated
+            return ThrowIllegalReferenceCountException(-decrement);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static int ThrowIllegalReferenceCountException(int increment)
+        {
+            throw GetIllegalReferenceCountException();
+
+            IllegalReferenceCountException GetIllegalReferenceCountException()
+            {
+                return new IllegalReferenceCountException(0, increment);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static void ThrowIllegalRawReferenceCountException(int count, int increment)
+        {
+            throw GetIllegalReferenceCountException();
+
+            IllegalReferenceCountException GetIllegalReferenceCountException()
+            {
+                return new IllegalReferenceCountException(RealRefCnt(count), increment);
+            }
         }
 
         protected internal abstract void Deallocate();
--- src/DotNetty.Common/Utilities/AbstractReferenceCounted.cs
+++ src/DotNetty.Common/Utilities/AbstractReferenceCounted.cs
@@ -6,9 +6,18 @@ namespace DotNetty.Common.Utilities
 
     public abstract class AbstractReferenceCounted : IReferenceCounted
     {
-        int referenceCount = 1;
+        // even => "real" refcount is (refCnt >>> 1); odd => "real" refcount is 0
+        int referenceCount = 2;
 
-        public int ReferenceCount => Volatile.Read(ref this.referenceCount);
+        public int ReferenceCount => RealRefCnt(Volatile.Read(ref this.referenceCount));
+
+        /// <summary>
+        /// An unsafe operation intended for use by a subclass that sets the reference count of the buffer directly
+        /// </summary>
+        protected void SetReferenceCount(int newRefCnt)
+        {
+            Interlocked.Exchange(ref this.referenceCount, newRefCnt << 1); // overflow OK here
+        }
 
         public IReferenceCounted Retain() => this.RetainCore(1);
 
@@ -21,18 +30,21 @@ namespace DotNetty.Common.Utilities
 
         protected virtual IReferenceCounted RetainCore(int increment)
         {
-            var refCnt = Volatile.Read(ref this.referenceCount);
-            int oldRefCnt;
-            do
+            // all changes to the raw count are 2x the "real" change
+            int adjustedIncrement = increment << 1; // overflow OK here
+
+            int oldRefCnt = this.GetAndAddRefCnt(adjustedIncrement);
+            if ((oldRefCnt & 1) != 0) { ThrowIllegalReferenceCountException(0, increment); }
+
+            int nextCount = oldRefCnt + adjustedIncrement;
+            // don't pass 0!
+            if ((oldRefCnt <= 0 && nextCount >= 0) ||
+                (oldRefCnt >= 0 && nextCount < oldRefCnt))
             {
-                oldRefCnt = refCnt;
-                int nextCount = refCnt + increment;
-
-                // Ensure we don't resurrect (which means the refCnt was 0) and also that we encountered an overflow.
-                if (nextCount <= increment) { ThrowIllegalReferenceCountException(refCnt, increment); }
-
-                refCnt = Interlocked.CompareExchange(ref this.referenceCount, nextCount, refCnt);
-            } while (refCnt != oldRefCnt);
+                // overflow case
+                this.GetAndAddRefCnt(-adjustedIncrement);
+                ThrowIllegalRawReferenceCountException(oldRefCnt, increment);
+            }
 
             return this;
         }
@@ -52,23 +64,117 @@ namespace DotNetty.Common.Utilities
 
         bool ReleaseCore(int decrement)
         {
+            int rawCnt = Volatile.Read(ref this.referenceCount);
+            int realCnt = ToLiveRealCnt(rawCnt, decrement);
+            if (decrement == realCnt)
+            {
+                if (this.CompareAndSetRefCnt(rawCnt, 1))
+                {
+                    this.Deallocate();
+                    return true;
+                }
+                return this.RetryRelease0(decrement);
+            }
+            return this.ReleaseNonFinal0(decrement, rawCnt, realCnt);
+        }
+
+        bool ReleaseNonFinal0(int decrement, int rawCnt, int realCnt)
+        {
+            if (decrement < realCnt
+                    // all changes to the raw count are 2x the "real" change
+                    && this.CompareAndSetRefCnt(rawCnt, rawCnt - (decrement << 1)))
+            {
+                return false;
+            }
+            return RetryRelease0(decrement);
+        }
+
+        bool RetryRelease0(int decrement)
+        {
+            var sw = new SpinWait();
+            int rawCnt = Volatile.Read(ref this.referenceCount);
+            int oldRawCnt;
+            while (true)
+            {
+                oldRawCnt = rawCnt;
+                int realCnt = ToLiveRealCnt(rawCnt, decrement);
+                if (decrement == realCnt)
+                {
+                    rawCnt = Interlocked.CompareExchange(ref this.referenceCount, 1, oldRawCnt);
+                    if (rawCnt == oldRawCnt)
+                    {
+                        this.Deallocate();
+                        return true;
+                    }
+                }
+                else if (decrement < realCnt)
+                {
+                    // all changes to the raw count are 2x the "real" change
+                    rawCnt = Interlocked.CompareExchange(ref this.referenceCount, rawCnt - (decrement << 1), oldRawCnt);
+                    if (rawCnt == oldRawCnt)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    ThrowIllegalReferenceCountException(realCnt, -decrement);
+                }
+                sw.SpinOnce(); // this benefits throughput under high contention
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        int GetAndAddRefCnt(int delta)
+        {
             var refCnt = Volatile.Read(ref this.referenceCount);
             int oldRefCnt;
             do
             {
                 oldRefCnt = refCnt;
-
-                if (refCnt < decrement) { ThrowIllegalReferenceCountException(refCnt, decrement); }
-
-                refCnt = Interlocked.CompareExchange(ref this.referenceCount, refCnt - decrement, refCnt);
+                refCnt = Interlocked.CompareExchange(ref this.referenceCount, refCnt + delta, oldRefCnt);
             } while (refCnt != oldRefCnt);
+            return oldRefCnt;
+        }
 
-            if (refCnt == decrement)
+        [MethodImpl(InlineMethod.Value)]
+        bool CompareAndSetRefCnt(int expect, int update)
+        {
+            return expect == Interlocked.CompareExchange(ref this.referenceCount, update, expect);
+        }
+
+        [MethodImpl(InlineMethod.Value)]
+        static int RealRefCnt(int rawCnt)
+        {
+            return (rawCnt & 1) != 0 ? 0 : rawCnt.RightUShift(1);
+        }
+
+        /// <summary>
+        /// Like <see cref="RealRefCnt(int)"/> but throws if refCnt == 0
+        /// </summary>
+        /// <param name="rawCnt"></param>
+        /// <param name="decrement"></param>
+        /// <returns></returns>
+        [MethodImpl(InlineMethod.Value)]
+        static int ToLiveRealCnt(int rawCnt, int decrement)
+        {
+            if ((rawCnt & 1) == 0)
             {
-                this.Deallocate();
-                return true;
+                return rawCnt.RightUShift(1);
             }
-            return false;
+            // odd rawCnt => already deallocated
+            return ThrowIllegalReferenceCountException(-decrement);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static int ThrowIllegalReferenceCountException(int increment)
+        {
+            throw GetIllegalReferenceCountException();
+
+            IllegalReferenceCountException GetIllegalReferenceCountException()
+            {
+                return new IllegalReferenceCountException(0, increment);
+            }
         }
 
         [MethodImpl(MethodImplOptions.NoInlining)]
@@ -79,6 +185,17 @@ namespace DotNetty.Common.Utilities
             IllegalReferenceCountException GetIllegalReferenceCountException()
             {
                 return new IllegalReferenceCountException(count, increment);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static void ThrowIllegalRawReferenceCountException(int count, int increment)
+        {
+            throw GetIllegalReferenceCountException();
+
+            IllegalReferenceCountException GetIllegalReferenceCountException()
+            {
+                return new IllegalReferenceCountException(RealRefCnt(count), increment);
             }
         }
 
