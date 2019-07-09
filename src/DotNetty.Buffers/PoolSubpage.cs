0d4838c511a844027455b2dc4a4002167f2b4935
SilverFox <yyjdelete@126.com>
2019-07-09 23:12:59 +0800

    Fix an spell error (#494)

--- src/DotNetty.Buffers/PoolSubpage.cs
+++ src/DotNetty.Buffers/PoolSubpage.cs
@@ -249,8 +249,8 @@ namespace DotNetty.Buffers
                 return "(" + this.memoryMapIdx + ": not in use)";
             }
 
-            return '(' + this.memoryMapIdx + ": " + (maxNumElems - numAvail) + '/' + maxNumElems +
-                ", offset: " + this.runOffset + ", length: " + this.pageSize + ", elemSize: " + elemSize + ')';
+            return "(" + this.memoryMapIdx + ": " + (maxNumElems - numAvail) + "/" + maxNumElems +
+                ", offset: " + this.runOffset + ", length: " + this.pageSize + ", elemSize: " + elemSize + ")";
         }
 
         public int MaxNumElements
