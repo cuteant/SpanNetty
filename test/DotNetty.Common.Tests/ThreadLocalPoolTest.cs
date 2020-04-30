Azure/DotNetty

Port all properties io.netty.recycler.* and related logic (#497)

test/DotNetty.Common.Tests/ThreadLocalPoolTest.cs
@@ -3,60 +3,128 @@
 
 namespace DotNetty.Common.Tests
 {
+    using DotNetty.Common.Concurrency;
     using System;
+    using System.Runtime.ExceptionServices;
+    using System.Threading;
     using System.Threading.Tasks;
     using Xunit;
 
     public class ThreadLocalPoolTest
     {
+        private static ThreadLocalPool<HandledObject> NewPool(int max)
+            => new ThreadLocalPool<HandledObject>(handle => new HandledObject(handle), max);
+
+        //Not use Task to do all `AtDifferentThreadTest`, because can't promise Task won't be run inline.
+
+        [Fact]
+        public void ThreadCanBeCollectedEvenIfHandledObjectIsReferencedTest()
+        {
+            ThreadLocalPool<HandledObject> pool = NewPool(1024);
+            HandledObject reference = null;
+            WeakReference<Thread> threadRef = null;
+            WeakReference<XThread> xThreadRef = null;
+
+            var thread1 = new Thread(() =>
+            {
+                //Don't know the reason, but thread2 will not be collected without wrapped with thread1
+                var thread2 = new Thread(() =>
+                {
+                    Volatile.Write(ref xThreadRef, new WeakReference<XThread>(XThread.CurrentThread));
+                    HandledObject data = pool.Take();
+                    // Store a reference to the HandledObject to ensure it is not collected when the run method finish.
+                    Volatile.Write(ref reference, data);
+                });
+                Volatile.Write(ref threadRef, new WeakReference<Thread>(thread2));
+                thread2.Start();
+                thread2.Join();
+                Assert.True(Volatile.Read(ref threadRef)?.TryGetTarget(out _));
+                Assert.True(Volatile.Read(ref xThreadRef)?.TryGetTarget(out _));
+
+                GC.KeepAlive(thread2);
+                // Null out so it can be collected.
+                thread2 = null;
+            });
+            thread1.Start();
+            thread1.Join();
+
+            for (int i = 0; i < 5; ++i)
+            {
+                GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true);
+                GC.WaitForPendingFinalizers();
+
+                if (Volatile.Read(ref threadRef)?.TryGetTarget(out _) == true || Volatile.Read(ref xThreadRef)?.TryGetTarget(out _) == true)
+                    Thread.Sleep(100);
+            }
+
+            Assert.False(Volatile.Read(ref threadRef)?.TryGetTarget(out _));
+            Assert.False(Volatile.Read(ref xThreadRef)?.TryGetTarget(out _));
+
+            // Now call recycle after the Thread was collected to ensure this still works...
+            reference.Release();
+            reference = null;
+        }
+
         [Fact]
         public void MultipleReleaseTest()
         {
-            RecyclableObject obj = RecyclableObject.NewInstance();
+            ThreadLocalPool<HandledObject> pool = NewPool(1024);
+            HandledObject obj = pool.Take();
             obj.Release();
             var exception = Assert.ThrowsAny<InvalidOperationException>(() => obj.Release());
+            Assert.True(exception != null);
+        }
+        [Fact]
+        public void MultipleReleaseAtDifferentThreadTest()
+        {
+            ThreadLocalPool<HandledObject> pool = NewPool(1024);
+            HandledObject obj = pool.Take();
+
+            Thread thread = new Thread(() =>
+            {
+                obj.Release();
+            });
+            thread.Start();
+            thread.Join();
+
+            ExceptionDispatchInfo exceptionDispatchInfo = null;
+            Thread thread2 = new Thread(() =>
+            {
+                try
+                {
+                    obj.Release();
+                }
+                catch (Exception ex)
+                {
+                    Volatile.Write(ref exceptionDispatchInfo, ExceptionDispatchInfo.Capture(ex));
+                }
+            });
+            thread2.Start();
+            thread2.Join();
+            var exception = Assert.ThrowsAny<InvalidOperationException>(() => Volatile.Read(ref exceptionDispatchInfo)?.Throw());
             Assert.True(exception != null);
         }
 
         [Fact]
         public void ReleaseTest()
         {
-            RecyclableObject obj = RecyclableObject.NewInstance();
+            ThreadLocalPool<HandledObject> pool = NewPool(1024);
+            HandledObject obj = pool.Take();
             obj.Release();
-            RecyclableObject obj2 = RecyclableObject.NewInstance();
+            HandledObject obj2 = pool.Take();
             Assert.Same(obj, obj2);
             obj2.Release();
         }
 
         [Fact]
-        public void RecycleAtDifferentThreadTest()
+        public void ReleaseDisableTest()
         {
-            RecyclableObject obj = RecyclableObject.NewInstance();
-
-            RecyclableObject prevObject = obj;
-            Task.Run(() => { obj.Release(); }).Wait();
-            obj = RecyclableObject.NewInstance();
-
-            Assert.True(obj == prevObject);
+            ThreadLocalPool<HandledObject> pool = NewPool(-1);
+            HandledObject obj = pool.Take();
             obj.Release();
-        }
-
-        class RecyclableObject
-        {
-            internal static readonly ThreadLocalPool<RecyclableObject> pool =
-                new ThreadLocalPool<RecyclableObject>(handle =>
-                    new RecyclableObject(handle), 1, true);
-
-            readonly ThreadLocalPool.Handle handle;
-
-            public RecyclableObject(ThreadLocalPool.Handle handle)
-            {
-                this.handle = handle;
-            }
-
-            public static RecyclableObject NewInstance() => pool.Take();
-
-            public void Release() => handle.Release(this);
+            HandledObject obj2 = pool.Take();
+            Assert.NotSame(obj, obj2);
+            obj2.Release();
         }
 
         class HandledObject
@@ -74,17 +142,17 @@ namespace DotNetty.Common.Tests
         [Fact]
         public void MaxCapacityTest()
         {
-            this.MaxCapacityTest0(300);
+            MaxCapacityTest0(300);
             var rand = new Random();
             for (int i = 0; i < 50; i++)
             {
-                this.MaxCapacityTest0(rand.Next(1000) + 256); // 256 - 1256
+                MaxCapacityTest0(rand.Next(1000) + 256); // 256 - 1256
             }
         }
 
-        void MaxCapacityTest0(int maxCapacity)
+        static void MaxCapacityTest0(int maxCapacity)
         {
-            var recycler = new ThreadLocalPool<HandledObject>(handle => new HandledObject(handle), maxCapacity);
+            var recycler = NewPool(maxCapacity);
 
             var objects = new HandledObject[maxCapacity * 3];
             for (int i = 0; i < objects.Length; i++)
@@ -97,14 +165,36 @@ namespace DotNetty.Common.Tests
                 objects[i].Release();
                 objects[i] = null;
             }
-            Assert.Equal(maxCapacity, recycler.ThreadLocalCapacity);
+            Assert.True(maxCapacity >= recycler.ThreadLocalCapacity,
+                "The threadLocalCapacity (" + recycler.ThreadLocalCapacity + ") must be <= maxCapacity ("
+                + maxCapacity + ") as we not pool all new handles internally");
         }
 
         [Fact]
-        public void MaxCapacityWithRecycleAtDifferentThreadTest()
+        public void ReleaseAtDifferentThreadTest()
+        {
+            ThreadLocalPool<HandledObject> pool = new ThreadLocalPool<HandledObject>(handle => new HandledObject(handle),
+                256, 10, 2, 10);
+
+            HandledObject obj = pool.Take();
+            HandledObject obj2 = pool.Take();
+            Thread thread = new Thread(() =>
+            {
+                obj.Release();
+                obj2.Release();
+            });
+            thread.Start();
+            thread.Join();
+
+            Assert.Same(pool.Take(), obj);
+            Assert.NotSame(pool.Take(), obj2);
+        }
+
+        [Fact]
+        public void MaxCapacityWithReleaseAtDifferentThreadTest()
         {
             const int maxCapacity = 4; // Choose the number smaller than WeakOrderQueue.LINK_CAPACITY
-            var recycler = new ThreadLocalPool<HandledObject>(handle => new HandledObject(handle), maxCapacity);
+            var pool = NewPool(maxCapacity);
 
             // Borrow 2 * maxCapacity objects.
             // Return the half from the same thread.
@@ -113,7 +203,7 @@ namespace DotNetty.Common.Tests
             var array = new HandledObject[maxCapacity * 3];
             for (int i = 0; i < array.Length; i++)
             {
-                array[i] = recycler.Take();
+                array[i] = pool.Take();
             }
 
             for (int i = 0; i < maxCapacity; i++)
@@ -121,24 +211,76 @@ namespace DotNetty.Common.Tests
                 array[i].Release();
             }
 
-            Task.Run(() =>
+            Thread thread = new Thread(() =>
             {
                 for (int i = maxCapacity; i < array.Length; i++)
                 {
                     array[i].Release();
                 }
-            }).Wait();
+            });
+            thread.Start();
+            thread.Join();
 
-            Assert.Equal(recycler.ThreadLocalCapacity, maxCapacity);
-            Assert.Equal(recycler.ThreadLocalSize, maxCapacity);
+            Assert.Equal(maxCapacity, pool.ThreadLocalCapacity);
+            Assert.Equal(1, pool.ThreadLocalSize);
 
             for (int i = 0; i < array.Length; i++)
             {
-                recycler.Take();
+                pool.Take();
             }
 
-            Assert.Equal(maxCapacity, recycler.ThreadLocalCapacity);
-            Assert.Equal(0, recycler.ThreadLocalSize);
+            Assert.Equal(maxCapacity, pool.ThreadLocalCapacity);
+            Assert.Equal(0, pool.ThreadLocalSize);
+        }
+
+        [Fact]
+        public void DiscardingExceedingElementsWithReleaseAtDifferentThreadTest()
+        {
+            int maxCapacity = 32;
+            int instancesCount = 0;
+
+            ThreadLocalPool<HandledObject> pool = new ThreadLocalPool<HandledObject>(handle =>
+            {
+                Interlocked.Increment(ref instancesCount);
+                return new HandledObject(handle);
+            }, maxCapacity, 2);
+
+            // Borrow 2 * maxCapacity objects.
+            HandledObject[] array = new HandledObject[maxCapacity * 2];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = pool.Take();
+            }
+
+            Assert.Equal(array.Length, Volatile.Read(ref instancesCount));
+            // Reset counter.
+            Volatile.Write(ref instancesCount, 0);
+
+            // Release from other thread.
+            Thread thread = new Thread(() =>
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i].Release();
+                }
+            });
+            thread.Start();
+            thread.Join();
+
+            Assert.Equal(0, Volatile.Read(ref instancesCount));
+
+            // Borrow 2 * maxCapacity objects. Half of them should come from
+            // the recycler queue, the other half should be freshly allocated.
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = pool.Take();
+            }
+
+            // The implementation uses maxCapacity / 2 as limit per WeakOrderQueue
+            Assert.True(array.Length - maxCapacity / 2 <= Volatile.Read(ref instancesCount),
+                "The instances count (" + Volatile.Read(ref instancesCount) + ") must be <= array.length (" + array.Length
+                + ") - maxCapacity (" + maxCapacity + ") / 2 as we not pool all new handles" +
+                " internally");
         }
     }
 }

cuteant/SpanNetty

test/DotNetty.Common.Tests/ThreadLocalPoolTest.cs
@@ -3,60 +3,128 @@
 
 namespace DotNetty.Common.Tests
 {
+    using DotNetty.Common.Concurrency;
     using System;
+    using System.Runtime.ExceptionServices;
+    using System.Threading;
     using System.Threading.Tasks;
     using Xunit;
 
     public class ThreadLocalPoolTest
     {
+        private static ThreadLocalPool<HandledObject> NewPool(int max)
+            => new ThreadLocalPool<HandledObject>(handle => new HandledObject(handle), max);
+
+        //Not use Task to do all `AtDifferentThreadTest`, because can't promise Task won't be run inline.
+
+        [Fact]
+        public void ThreadCanBeCollectedEvenIfHandledObjectIsReferencedTest()
+        {
+            ThreadLocalPool<HandledObject> pool = NewPool(1024);
+            HandledObject reference = null;
+            WeakReference<Thread> threadRef = null;
+            WeakReference<XThread> xThreadRef = null;
+
+            var thread1 = new Thread(() =>
+            {
+                //Don't know the reason, but thread2 will not be collected without wrapped with thread1
+                var thread2 = new Thread(() =>
+                {
+                    Interlocked.Exchange(ref xThreadRef, new WeakReference<XThread>(XThread.CurrentThread));
+                    HandledObject data = pool.Take();
+                    // Store a reference to the HandledObject to ensure it is not collected when the run method finish.
+                    Interlocked.Exchange(ref reference, data);
+                });
+                Interlocked.Exchange(ref threadRef, new WeakReference<Thread>(thread2));
+                thread2.Start();
+                thread2.Join();
+                Assert.True(Volatile.Read(ref threadRef)?.TryGetTarget(out _));
+                Assert.True(Volatile.Read(ref xThreadRef)?.TryGetTarget(out _));
+
+                GC.KeepAlive(thread2);
+                // Null out so it can be collected.
+                thread2 = null;
+            });
+            thread1.Start();
+            thread1.Join();
+
+            for (int i = 0; i < 5; ++i)
+            {
+                GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true);
+                GC.WaitForPendingFinalizers();
+
+                if (Volatile.Read(ref threadRef)?.TryGetTarget(out _) == true || Volatile.Read(ref xThreadRef)?.TryGetTarget(out _) == true)
+                    Thread.Sleep(100);
+            }
+
+            Assert.False(Volatile.Read(ref threadRef)?.TryGetTarget(out _));
+            Assert.False(Volatile.Read(ref xThreadRef)?.TryGetTarget(out _));
+
+            // Now call recycle after the Thread was collected to ensure this still works...
+            reference.Release();
+            reference = null;
+        }
+
         [Fact]
         public void MultipleReleaseTest()
         {
-            RecyclableObject obj = RecyclableObject.NewInstance();
+            ThreadLocalPool<HandledObject> pool = NewPool(1024);
+            HandledObject obj = pool.Take();
             obj.Release();
             var exception = Assert.ThrowsAny<InvalidOperationException>(() => obj.Release());
+            Assert.True(exception != null);
+        }
+        [Fact]
+        public void MultipleReleaseAtDifferentThreadTest()
+        {
+            ThreadLocalPool<HandledObject> pool = NewPool(1024);
+            HandledObject obj = pool.Take();
+
+            Thread thread = new Thread(() =>
+            {
+                obj.Release();
+            });
+            thread.Start();
+            thread.Join();
+
+            ExceptionDispatchInfo exceptionDispatchInfo = null;
+            Thread thread2 = new Thread(() =>
+            {
+                try
+                {
+                    obj.Release();
+                }
+                catch (Exception ex)
+                {
+                    Interlocked.Exchange(ref exceptionDispatchInfo, ExceptionDispatchInfo.Capture(ex));
+                }
+            });
+            thread2.Start();
+            thread2.Join();
+            var exception = Assert.ThrowsAny<InvalidOperationException>(() => Volatile.Read(ref exceptionDispatchInfo)?.Throw());
             Assert.True(exception != null);
         }
 
         [Fact]
         public void ReleaseTest()
         {
-            RecyclableObject obj = RecyclableObject.NewInstance();
+            ThreadLocalPool<HandledObject> pool = NewPool(1024);
+            HandledObject obj = pool.Take();
             obj.Release();
-            RecyclableObject obj2 = RecyclableObject.NewInstance();
+            HandledObject obj2 = pool.Take();
             Assert.Same(obj, obj2);
             obj2.Release();
         }
 
         [Fact]
-        public void RecycleAtDifferentThreadTest()
+        public void ReleaseDisableTest()
         {
-            RecyclableObject obj = RecyclableObject.NewInstance();
-
-            RecyclableObject prevObject = obj;
-            Task.Run(() => { obj.Release(); }).Wait();
-            obj = RecyclableObject.NewInstance();
-
-            Assert.True(obj == prevObject);
+            ThreadLocalPool<HandledObject> pool = NewPool(-1);
+            HandledObject obj = pool.Take();
             obj.Release();
-        }
-
-        class RecyclableObject
-        {
-            internal static readonly ThreadLocalPool<RecyclableObject> pool =
-                new ThreadLocalPool<RecyclableObject>(handle =>
-                    new RecyclableObject(handle), 1, true);
-
-            readonly ThreadLocalPool.Handle handle;
-
-            public RecyclableObject(ThreadLocalPool.Handle handle)
-            {
-                this.handle = handle;
-            }
-
-            public static RecyclableObject NewInstance() => pool.Take();
-
-            public void Release() => handle.Release(this);
+            HandledObject obj2 = pool.Take();
+            Assert.NotSame(obj, obj2);
+            obj2.Release();
         }
 
         class HandledObject
@@ -74,17 +142,17 @@ namespace DotNetty.Common.Tests
         [Fact]
         public void MaxCapacityTest()
         {
-            this.MaxCapacityTestInternal(300);
+            MaxCapacityTest0(300);
             var rand = new Random();
             for (int i = 0; i < 50; i++)
             {
-                this.MaxCapacityTestInternal(rand.Next(1000) + 256); // 256 - 1256
+                MaxCapacityTest0(rand.Next(1000) + 256); // 256 - 1256
             }
         }
 
-        void MaxCapacityTestInternal(int maxCapacity)
+        static void MaxCapacityTest0(int maxCapacity)
         {
-            var recycler = new ThreadLocalPool<HandledObject>(handle => new HandledObject(handle), maxCapacity);
+            var recycler = NewPool(maxCapacity);
 
             var objects = new HandledObject[maxCapacity * 3];
             for (int i = 0; i < objects.Length; i++)
@@ -97,14 +165,36 @@ namespace DotNetty.Common.Tests
                 objects[i].Release();
                 objects[i] = null;
             }
-            Assert.Equal(maxCapacity, recycler.ThreadLocalCapacity);
+            Assert.True(maxCapacity >= recycler.ThreadLocalCapacity,
+                "The threadLocalCapacity (" + recycler.ThreadLocalCapacity + ") must be <= maxCapacity ("
+                + maxCapacity + ") as we not pool all new handles internally");
         }
 
         [Fact]
-        public void MaxCapacityWithRecycleAtDifferentThreadTest()
+        public void ReleaseAtDifferentThreadTest()
+        {
+            ThreadLocalPool<HandledObject> pool = new ThreadLocalPool<HandledObject>(handle => new HandledObject(handle),
+                256, 10, 2, 10);
+
+            HandledObject obj = pool.Take();
+            HandledObject obj2 = pool.Take();
+            Thread thread = new Thread(() =>
+            {
+                obj.Release();
+                obj2.Release();
+            });
+            thread.Start();
+            thread.Join();
+
+            Assert.Same(pool.Take(), obj);
+            Assert.NotSame(pool.Take(), obj2);
+        }
+
+        [Fact]
+        public void MaxCapacityWithReleaseAtDifferentThreadTest()
         {
             const int maxCapacity = 4; // Choose the number smaller than WeakOrderQueue.LINK_CAPACITY
-            var recycler = new ThreadLocalPool<HandledObject>(handle => new HandledObject(handle), maxCapacity);
+            var pool = NewPool(maxCapacity);
 
             // Borrow 2 * maxCapacity objects.
             // Return the half from the same thread.
@@ -113,7 +203,7 @@ namespace DotNetty.Common.Tests
             var array = new HandledObject[maxCapacity * 3];
             for (int i = 0; i < array.Length; i++)
             {
-                array[i] = recycler.Take();
+                array[i] = pool.Take();
             }
 
             for (int i = 0; i < maxCapacity; i++)
@@ -121,24 +211,76 @@ namespace DotNetty.Common.Tests
                 array[i].Release();
             }
 
-            Task.Run(() =>
+            Thread thread = new Thread(() =>
             {
                 for (int i = maxCapacity; i < array.Length; i++)
                 {
                     array[i].Release();
                 }
-            }).Wait();
+            });
+            thread.Start();
+            thread.Join();
 
-            Assert.Equal(recycler.ThreadLocalCapacity, maxCapacity);
-            Assert.Equal(recycler.ThreadLocalSize, maxCapacity);
+            Assert.Equal(maxCapacity, pool.ThreadLocalCapacity);
+            Assert.Equal(1, pool.ThreadLocalSize);
 
             for (int i = 0; i < array.Length; i++)
             {
-                recycler.Take();
+                pool.Take();
             }
 
-            Assert.Equal(maxCapacity, recycler.ThreadLocalCapacity);
-            Assert.Equal(0, recycler.ThreadLocalSize);
+            Assert.Equal(maxCapacity, pool.ThreadLocalCapacity);
+            Assert.Equal(0, pool.ThreadLocalSize);
+        }
+
+        [Fact]
+        public void DiscardingExceedingElementsWithReleaseAtDifferentThreadTest()
+        {
+            int maxCapacity = 32;
+            int instancesCount = 0;
+
+            ThreadLocalPool<HandledObject> pool = new ThreadLocalPool<HandledObject>(handle =>
+            {
+                Interlocked.Increment(ref instancesCount);
+                return new HandledObject(handle);
+            }, maxCapacity, 2);
+
+            // Borrow 2 * maxCapacity objects.
+            HandledObject[] array = new HandledObject[maxCapacity * 2];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = pool.Take();
+            }
+
+            Assert.Equal(array.Length, Volatile.Read(ref instancesCount));
+            // Reset counter.
+            Interlocked.Exchange(ref instancesCount, 0);
+
+            // Release from other thread.
+            Thread thread = new Thread(() =>
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i].Release();
+                }
+            });
+            thread.Start();
+            thread.Join();
+
+            Assert.Equal(0, Volatile.Read(ref instancesCount));
+
+            // Borrow 2 * maxCapacity objects. Half of them should come from
+            // the recycler queue, the other half should be freshly allocated.
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = pool.Take();
+            }
+
+            // The implementation uses maxCapacity / 2 as limit per WeakOrderQueue
+            Assert.True(array.Length - maxCapacity / 2 <= Volatile.Read(ref instancesCount),
+                "The instances count (" + Volatile.Read(ref instancesCount) + ") must be <= array.length (" + array.Length
+                + ") - maxCapacity (" + maxCapacity + ") / 2 as we not pool all new handles" +
+                " internally");
         }
     }
 }