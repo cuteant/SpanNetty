8f1111d22a93c7be1a5f16194f37e037ef998ae8
Maxim Kim <mkim@microsoft.com>
2023-02-07 22:25:50 -0800

    Revert "Fix CompositeByteBuffer.ComponentAtOffset() (#85)"
    This reverts commit 7eccf0b9392765e6599d23419979a6de65125233.

--- src/DotNetty.Buffers/CompositeByteBuffer.cs
+++ src/DotNetty.Buffers/CompositeByteBuffer.cs
@@ -112,7 +112,7 @@ namespace DotNetty.Buffers
 
             public IByteBuffer Duplicate()
             {
-                return SrcBuffer.Duplicate().SetIndex(SrcIdx(Offset), SrcIdx(EndOffset));
+                return SrcBuffer.Duplicate();
             }
 
             public void Free()
